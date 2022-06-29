4c084059ea5ccb3f6b5312fe0343f2581d51d2de
Euclides <euclideskinto@gmail.com.br>
2022-06-29 11:04:44 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- Content -->
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner/>
+
+      <TodoFormAdd/>
+
+      <TodoItems/>
+
+      <TodoEmpty/>
+    </div>
+  </div>
+  <!--/ Content -->
+</template>
+
+<script>
+import  TodoSpinner  from '@/components/TodoSpinner';
+import TodoFormAdd from '@/components/TodoFormAdd.vue';
+import TodoItems from '@/components/TodoItems';
+import TodoEmpty from '@/components/TodoEmpty';
+export default {
+
+  name: 'App',
+  components: {
+    TodoSpinner,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty
+}
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="space-y-2">
+    <TodoItem/>
+  </div>
+</template>
+<script>
+import TodoItem from '@/components/TodoItem.vue';
+
+export default {
+  name: 'TodoList',
+  components: {
+    TodoItem
+  }
+}
+</script>
