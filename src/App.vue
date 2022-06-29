8d9f8220e72799734cda4ddbfe3365c3b445d4a7
Euclides <euclideskinto@gmail.com.br>
2022-06-29 13:01:45 -0300

    add axios and vuex

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <!-- Content -->
   <div class="px-3 py-10 md:px-10">
     <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
-      <TodoSpinner/>
+      <TodoSpinner v-if="loading"/>
 
-      <TodoFormAdd/>
+      <template>
+        <TodoFormAdd/>
 
-      <TodoItems/>
+        <TodoItems/>
 
-      <TodoEmpty/>
+        <TodoEmpty/>
+      </template>
     </div>
   </div>
   <!--/ Content -->
@@ -19,6 +21,8 @@ import  TodoSpinner  from '@/components/TodoSpinner';
 import TodoFormAdd from '@/components/TodoFormAdd.vue';
 import TodoItems from '@/components/TodoItems';
 import TodoEmpty from '@/components/TodoEmpty';
+import axios from 'axios';
+
 export default {
 
   name: 'App',
@@ -27,7 +31,26 @@ export default {
     TodoFormAdd,
     TodoItems,
     TodoEmpty
-}
+  },
+  data() {
+    return {
+      todos: [],
+      loading: false,
+      error: null
+    };
+  },
+  created(){
+    this.loading = true;
+    axios.get('http://localhost:3000/todos')
+    .then((response) =>{
+      this.$store.commit('storeTodos', response.data);
+    })
+    .finally(() => {
+      this.loading = false;
+    });
+  },
+
+
 }
 </script>
 
